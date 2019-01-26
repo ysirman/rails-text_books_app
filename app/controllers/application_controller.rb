f5b77e1d1ca69b89f249dcc74fb6f3dee1822175
ysirman <24975537+ysirman@users.noreply.github.com>
2019-01-27 00:18:25 +0900

    deviseによる認証機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    before_action :set_locale
+    before_action :set_locale, :authenticate_user!
     def set_locale
         I18n.locale = params[:locale] || I18n.default_locale
     end
