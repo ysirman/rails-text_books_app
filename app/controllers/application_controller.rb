88d6eac6adf1827066ea613759f303f66e055bc2
ysirman <24975537+ysirman@users.noreply.github.com>
2019-02-09 12:55:07 +0900

    ActiveStorageによる画像アップロード機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,16 @@ class ApplicationController < ActionController::Base
     def default_url_options
         { locale: I18n.locale }
     end
+
+    # deviseコントローラーにストロングパラメータを追加     
+    before_action :configure_permitted_parameters, if: :devise_controller?
+  
+    protected
+    def configure_permitted_parameters
+        added_attrs = [:name, :avatar]
+        # サインアップ時のストロングパラメータ
+        devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+        # アカウント編集時のストロングパラメータ
+        devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    end
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # uid=nil防止のため、ユーザー登録（deviseのcreateアクション）で使用するメソッドを上書き
-  def build_resource(hash = nil)
+  def build_resource(hash = {})
     hash[:uid] = User.create_unique_string
     super
   end
