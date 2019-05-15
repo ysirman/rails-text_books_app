f2be69aa8cc4f20c0b4b54d4e25585c3c2b57a33
ysirman <24975537+ysirman@users.noreply.github.com>
2019-05-15 22:28:24 +0900

    RSpecセットアップ

--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,11 @@ module BooksApp
     # the framework and any gems in your application.
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.generators do |g|
+      g.test_framework :rspec,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: false
+    end
   end
 end
